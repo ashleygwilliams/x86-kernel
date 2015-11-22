ded1cbdc52a7a54df9a8c4c1be8cb3a798b29416
Ashley Williams <ashley666ashley@gmail.com>
2015-11-22 16:51:24 -0500

    add tests for multiboot, cpuid, and long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,8 +3,69 @@ global start
 section .text
 bits 32
 start:
+  mov esp, stack_top 
+
+  ; tests
+  call test_multiboot
+  call test_cpuid
+  call test_long_mode
+
   ; print OK to the screen
   ; 0xb8000 is the upper left chunk of the screen (convention)
   ; 0x2f4b2f4f is ascii for OK
   mov dword [0xb8000], 0x2f4b2f4f
   hlt
+
+; prints `ERR: ` + error code 
+; parameter: error code (in ascii) in al
+error:
+  mov dword [0xb8000], 0x4f524f45
+  mov dword [0xb8004], 0x4f3a4f52
+  mov dword [0xb8008], 0xff204f20
+  mov byte [0xb800a], al
+  hlt
+
+test_multiboot:
+  cmp eax, 0x36d76289
+  jne .no_multiboot
+  ret
+.no_multiboot:
+  mov al, "0"
+  jmp error
+
+test_cpuid:
+    pushfd               ; Store the FLAGS-register.
+    pop eax              ; Restore the A-register.
+    mov ecx, eax         ; Set the C-register to the A-register.
+    xor eax, 1 << 21     ; Flip the ID-bit, which is bit 21.
+    push eax             ; Store the A-register.
+    popfd                ; Restore the FLAGS-register.
+    pushfd               ; Store the FLAGS-register.
+    pop eax              ; Restore the A-register.
+    push ecx             ; Store the C-register.
+    popfd                ; Restore the FLAGS-register.
+    xor eax, ecx         ; Do a XOR-operation on the A-register and the C-register.
+    jz .no_cpuid         ; The zero flag is set, no CPUID.
+    ret                  ; CPUID is available for use.
+.no_cpuid:
+    mov al, "1"
+    jmp error
+
+test_long_mode:
+    mov eax, 0x80000000    ; Set the A-register to 0x80000000.
+    cpuid                  ; CPU identification.
+    cmp eax, 0x80000001    ; Compare the A-register with 0x80000001.
+    jb .no_long_mode       ; It is less, there is no long mode.
+    mov eax, 0x80000001    ; Set the A-register to 0x80000001.
+    cpuid                  ; CPU identification.
+    test edx, 1 << 29      ; Test if the LM-bit, which is bit 29, is set in the D-register.
+    jz .no_long_mode       ; They aren't, there is no long mode.
+    ret
+.no_long_mode:
+    mov al, "2"
+    jmp error
+
+section .bss
+stack_bottom:
+  resb 64
+stack_top:

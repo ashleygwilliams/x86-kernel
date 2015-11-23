f5327837948c9b3a5d5506074cb90e44b2cb710a
Ashley Williams <ashley666ashley@gmail.com>
2015-11-22 20:10:05 -0500

    hello world from rust

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,18 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-  // fixed by #rlibc
-  let v = ["huge", "page", "long", "mode"];
+  let hello = b"Hello World!";
+  let color_byte = 0x1f;
 
-  // fixed by -Z no-landing-pads
-  let test = (0..3).flat_map(|x| 0..x).zip(0..);
+  let mut hello_colored = [color_byte; 24];
+  for (i, char_byte) in hello.into_iter().enumerate() {
+    hello_colored[i * 2] = *char_byte;
+  }
 
-  let mut a = 42;
-  a += 1;
+  let buffer_ptr = (0xb8000  +1988) as *mut _;
+  unsafe { *buffer_ptr = hello_colored };
+
+  loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}

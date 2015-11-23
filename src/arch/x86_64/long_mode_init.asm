78d51ef41a9115d4fab2fe2856789909cc33a21e
Ashley Williams <ashley666ashley@gmail.com>
2015-11-22 19:50:13 -0500

    enable SSE
    - SSE is single instruction multiple data
    - wrote some code to enable sse
    - wrote at test to ensure os doesn't boot w/no see

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -4,8 +4,45 @@ section .text
 bits 64
 long_mode_start:
   extern rust_main
+  call setup_SSE
   call rust_main 
 
-  mov rax, 0x2f592f412f4b2f4f
-  mov qword [0xb8000], rax
-  hlt
+.os_returned:
+    ; rust main returned, print `OS returned!`
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
+    hlt
+
+error:
+    mov rbx, 0x4f4f4f524f524f45
+    mov [0xb8000], rbx
+    mov rbx, 0x4f204f204f3a4f52
+    mov [0xb8008], rbx
+    mov byte [0xb800e], al
+    hlt
+    jmp error
+
+setup_SSE:
+    ; check for SSE
+    mov rax, 0x1
+    cpuid
+    test edx, 1<<25
+    jz .no_SSE
+
+    ; enable SSE
+    mov rax, cr0
+    and ax, 0xFFFB      ; clear coprocessor emulation CR0.EM
+    or ax, 0x2          ; set coprocessor monitoring  CR0.MP
+    mov cr0, rax
+    mov rax, cr4
+    or ax, 3 << 9       ; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, rax
+
+    ret
+.no_SSE:
+    mov al, "a"
+    jmp error
--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,14 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
+  // fixed by #rlibc
   let v = ["huge", "page", "long", "mode"];
+
+  // fixed by -Z no-landing-pads
   let test = (0..3).flat_map(|x| 0..x).zip(0..);
+
+  let mut a = 42;
+  a += 1;
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}

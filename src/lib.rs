8741e7391e4489f6641fa2b5019d9cfec5f28832
Ashley Williams <ashley666ashley@gmail.com>
2015-11-22 19:15:11 -0500

    fix errors in rust due to bare metal
    - add rlibc to give mem* funcs
    - add gc sections (hack to remove need for floating pt operations)
    - update linker script to keep multiboot_header at top

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@
 #![feature(lang_items)]
 #![no_std]
 
+extern crate rlibc;
+
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn rust_main() {
+  let v = ["huge", "page", "long", "mode"];
+}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] extern fn panic_fmt() -> ! { loop{} }

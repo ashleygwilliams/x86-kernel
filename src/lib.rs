5d96bcc5fc8538b6f6722f7715ebe6252e956b16
Rex Lunae <rexlunae@gmail.com>
2016-09-11 07:50:31 -0700

    Added _Resume_Unwind to work around useless calls inserted by the rust compiler.

--- src/lib.rs
+++ src/lib.rs
@@ -19,5 +19,11 @@ pub extern fn rust_main() {
   loop{}
 }
 
+// This is required by because Rust creates some references to it even if we disable it.
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
+        loop {}
+}
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] extern fn panic_fmt() -> ! { loop{} }
